de77e9098268b01a02a3480eaa268c481cceb7a0
Rodrigo Dumont <rlsdumont@gmail.com>
2019-06-21 17:54:38 -0300

    Port original client implementation

---
+++ index.js
@@ -0,0 +1,51 @@
+const redis = require('redis')
+const { promisify } = require('util')
+
+const { getSession } = require('./lib/sessions')
+const { listExperiments, createExperiment, getExperiment } = require('./lib/experiments')
+const { listFlags, setFlag } = require('./lib/flags')
+
+class TogClient {
+  constructor (redisUrl) {
+    this.redisClient = redis.createClient(redisUrl)
+    this.redisClient.on('error', err => console.log('error') || console.error(err))
+    this.redis = {
+      keys: promisify(this.redisClient.keys).bind(this.redisClient),
+      get: promisify(this.redisClient.get).bind(this.redisClient),
+      hgetall: promisify(this.redisClient.hgetall).bind(this.redisClient),
+      hmset: promisify(this.redisClient.hmset).bind(this.redisClient),
+      set: promisify(this.redisClient.set).bind(this.redisClient),
+      expire: promisify(this.redisClient.expire).bind(this.redisClient)
+    }
+  }
+
+  /**
+   * @param {string} namespace
+   * @returns {Object.<string, boolean>}
+   */
+  listFlags (namespace) {
+    return listFlags(this.redis, namespace)
+  }
+
+  setFlag (namespace, name, state, description) {
+    return setFlag(this.redis, namespace, name, state, description)
+  }
+
+  getSession (namespace, id, expiration) {
+    return getSession(this.redis, namespace, id, expiration)
+  }
+
+  listExperiments (namespace) {
+    return listExperiments(this.redis, namespace)
+  }
+
+  getExperiment (namespace, name) {
+    return getExperiment(this.redis, namespace, name)
+  }
+
+  createExperiment (namespace, name, weight, flags) {
+    return createExperiment(this.redis, namespace, name, weight, flags)
+  }
+}
+
+module.exports = TogClient
